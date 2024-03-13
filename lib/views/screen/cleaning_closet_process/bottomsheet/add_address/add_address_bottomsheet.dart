f9f65d5c58d96aaf64bf0dcd3a08d3d941f91e89
ByteAurora <byteaurora.dev@gmail.com>
2024-03-13 23:44:13 +0900

    feat: 옷장정리 신청 진행 화면 내 bottomsheet, page 추가

---
+++ lib/views/screen/cleaning_closet_process/bottomsheet/add_address/add_address_bottomsheet.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddAddressBottomSheet extends StatefulWidget {
+  const AddAddressBottomSheet({super.key});
+
+  @override
+  State<AddAddressBottomSheet> createState() => _AddAddressBottomSheetState();
+}
+
+class _AddAddressBottomSheetState extends State<AddAddressBottomSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Add Address Bottom Sheet'));
+  }
+}
---
+++ lib/views/screen/cleaning_closet_process/page/pickup_type/pickup_type_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PickupTypePage extends StatefulWidget {
+  const PickupTypePage({super.key});
+
+  @override
+  State<PickupTypePage> createState() => _PickupTypePageState();
+}
+
+class _PickupTypePageState extends State<PickupTypePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Pickup Type Page'));
+  }
+}
